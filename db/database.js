3486516def8397ee19427640c7245937b28afec1
Ruibo Xu <ruibo.xu@stonybrook.edu>
2022-10-23 23:13:01 -0400

    test

--- db/database.js
+++ db/database.js
@@ -3,12 +3,13 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 dotenv.config();
 
+const uri = "mongodb+srv://tileworkshop:twscse416@cluster0.nqqoxyo.mongodb.net/?retryWrites=true&w=majority";
 mongoose
-    .connect(process.env.DB_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true })
+    .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
     .catch(e => {
         console.error('Connection error', e.message)
     })
-
+    
 const db = mongoose.connection
 
 module.exports = db
