3944ce21c571688dc8de376af120de1b47144c3b
XeenProof <whosejimmy@gmail.com>
2022-10-24 15:00:16 -0400

    email + username check confirmed

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -293,7 +293,7 @@ registerUser = async (req, res) => {
                 })
         }
         console.log("password and password verify match");
-        const existingUser = await User.findOne({$or:[{ email: email },{username: username}]});
+        const existingUser = await User.findOne({ email: email });
         console.log("existingUser: " + existingUser);
         if (existingUser) {
             return res
@@ -301,6 +301,17 @@ registerUser = async (req, res) => {
                 .json({
                     success: false,
                     errorMessage: "An account with this email address already exists."
+                })
+        }
+
+        const existingUser2 = await User.findOne({username: username});
+        console.log("existingUser: " + existingUser2);
+        if (existingUser2) {
+            return res
+                .status(400)
+                .json({
+                    success: false,
+                    errorMessage: "An account with this username already exists."
                 })
         }
 
