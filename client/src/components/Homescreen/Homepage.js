5e355c0994b536fb9e08a7c3003937e3a3607295
XeenProof <whosejimmy@gmail.com>
2022-10-28 18:47:00 -0400

    MainNavbar Completed

---
+++ client/src/components/Homescreen/Homepage.js
@@ -0,0 +1,39 @@
+import React, { useContext, useEffect } from 'react'
+import MainNavbar from '../Navbars/MainNavbar';
+//import { GlobalStoreContext } from '../store'
+//import ListCard from './ListCard.js'
+//import { Fab, Typography } from '@mui/material'
+//import AddIcon from '@mui/icons-material/Add';
+//import List from '@mui/material/List';
+const HomePage = () => {
+    /*
+    const { store } = useContext(GlobalStoreContext);
+
+    useEffect(() => {
+        store.loadIdNamePairs();
+    }, []);
+
+    function handleCreateNewList() {
+        store.createNewList();
+    }
+    let listCard = "";
+    if (store) {
+        listCard = 
+            <List sx={{ width: '90%', left: '5%', bgcolor: 'background.paper' }}>
+            {
+                store.idNamePairs.map((pair) => (
+                    <ListCard
+                        key={pair._id}
+                        idNamePair={pair}
+                        selected={false}
+                    />
+                ))
+            }
+            </List>;
+    }
+    */
+    return (<div id="TWS-home-page">
+        Hello
+    </div>)
+}
+export default HomePage;
--- client/src/components/Homescreen/Homescreen.js
+++ client/src/components/Homescreen/Homescreen.js
@@ -1,44 +1,17 @@
 import React, { useContext, useEffect } from 'react'
-import TopBanner from '../Navbars/TopBanner';
+import MainNavbar from '../Navbars/MainNavbar';
+import { Container } from '@chakra-ui/react';
 //import { GlobalStoreContext } from '../store'
 //import ListCard from './ListCard.js'
 //import { Fab, Typography } from '@mui/material'
 //import AddIcon from '@mui/icons-material/Add';
 //import List from '@mui/material/List';
-const HomePage = () => {
-    /*
-    const { store } = useContext(GlobalStoreContext);
-
-    useEffect(() => {
-        store.loadIdNamePairs();
-    }, []);
-
-    function handleCreateNewList() {
-        store.createNewList();
-    }
-    let listCard = "";
-    if (store) {
-        listCard = 
-            <List sx={{ width: '90%', left: '5%', bgcolor: 'background.paper' }}>
-            {
-                store.idNamePairs.map((pair) => (
-                    <ListCard
-                        key={pair._id}
-                        idNamePair={pair}
-                        selected={false}
-                    />
-                ))
-            }
-            </List>;
-    }
-    */
+const Homescreen = () => {
     return (
         <div>
-            <div><TopBanner/></div>
-            <div id="TWS-home-page">
-                Hello This sucks
-            </div>
+            <MainNavbar/>
+            Some test
         </div>)
 }
 
-export default HomePage;
+export default Homescreen;
