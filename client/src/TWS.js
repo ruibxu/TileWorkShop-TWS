2d0b4ddbee649a66032795dc6820574f3689bc64
Ruibo Xu <ruibo.xu@stonybrook.edu>
2022-10-29 21:34:45 -0400

    url fix

--- client/src/TWS.js
+++ client/src/TWS.js
@@ -4,9 +4,9 @@ import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 //import { AuthContextProvider } from './auth';
 //import { GlobalStoreContextProvider } from './store'
 import Homescreen from './components/Homescreen/Homescreen.js';
-import Listscreen from './components/Listscreen/Listscreen';
-import EditTileSetScreen from './components/EditTileSetScreen/EditTileSetScreen';
-import EditTileMapScreen from './components/EditTileMapScreen/EditTileMapScreen';
+import Listscreen from './components/Listscreen/Listscreen.js';
+import EditTileSetScreen from './components/EditTileSetScreen/EditTileSetScreen.js';
+import EditTileMapScreen from './components/EditTileMapScreen/EditTileMapScreen.js';
 
 
 const TWS = () => {
@@ -14,10 +14,10 @@ const TWS = () => {
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={() => <Homescreen/>} /> 
-                <Route path="/homescreen/" exact component={() => <Homescreen/>} /> 
-                <Route path="/listscreen/" exact component={() => <Listscreen/>} /> 
-                <Route path="/tileset/" exact component={() => <EditTileSetScreen/>}/>
-                <Route path="/tilemap/" exact component={() => <EditTileMapScreen/>}/>
+                <Route path="/homescreen" exact component={() => <Homescreen/>} /> 
+                <Route path="/listscreen" exact component={() => <Listscreen/>} /> 
+                <Route path="/tileset" exact component={() => <EditTileSetScreen/>}/>
+                <Route path="/tilemap" exact component={() => <EditTileMapScreen/>}/>
             </Switch>
         </BrowserRouter>
     )
