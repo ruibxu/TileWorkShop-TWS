1ad6744ea551de8322fca305db7f086b4f4909b2
XeenProof <whosejimmy@gmail.com>
2022-10-30 18:06:11 -0400

    Update Account Modal Completed

--- client/src/components/Listscreen/Listscreen.js
+++ client/src/components/Listscreen/Listscreen.js
@@ -7,10 +7,11 @@ import { useDisclosure } from '@chakra-ui/react';
 import ListscreenSideBar from './ListscreenSideBar';
 import ListscreenMain from './ListscreenMain';
 import HomescreenNew from '../Homescreen/HomescreenNew';
-import ForgetPasswordModal from '../Modals/ForgetPassword-Modal';
 
 import SignUpModal from '../Modals/SignUp-Modal';
 import LoginModal from '../Modals/Login-Modal';
+import ForgetPasswordModal from '../Modals/ForgetPassword-Modal';
+import UpdateAccountModal from '../Modals/UpdateAccount-Modal';
 //import { GlobalStoreContext } from '../store'
 //import ListCard from './ListCard.js'
 //import { Fab, Typography } from '@mui/material'
@@ -25,10 +26,14 @@ const Listscreen = () => {
     const showSignUpModal = useDisclosure()
     const showLoginModal = useDisclosure()
     const showForgetPasswordModal = useDisclosure()
+    const showUpdateAccountModal = useDisclosure()
 
     return (
         <div className='overlay'>
-            <MainNavbar redirect={redirect} openSignUpModal={showSignUpModal.onOpen} openLoginModal={showLoginModal.onOpen}/>
+            <MainNavbar redirect={redirect} 
+                openSignUpModal={showSignUpModal.onOpen} openLoginModal={showLoginModal.onOpen}
+                openUpdateAccountModal={showUpdateAccountModal.onOpen}
+            />
             <Box height={'100%'} width={'100%'}>
                 <Flex height={'100%'}>
                     <ListscreenSideBar/>
@@ -40,6 +45,7 @@ const Listscreen = () => {
             <SignUpModal isOpen={showSignUpModal.isOpen} onClose={showSignUpModal.onClose}/>
             <LoginModal isOpen={showLoginModal.isOpen} onClose={showLoginModal.onClose} openForgetPasswordModal={showForgetPasswordModal.onOpen}/>
             <ForgetPasswordModal isOpen={showForgetPasswordModal.isOpen} onClose={showForgetPasswordModal.onClose}/>
+            <UpdateAccountModal isOpen={showUpdateAccountModal.isOpen} onClose={showUpdateAccountModal.onClose}/>
         </div>)
 }
 
