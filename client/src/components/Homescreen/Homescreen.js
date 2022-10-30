5b2c0515cac3a2a8b1204793ea6ee54035299542
bladexyzz <yibo.hu@stonybrook.edu>
2022-10-30 16:41:25 -0400

    edit button

--- client/src/components/Homescreen/Homescreen.js
+++ client/src/components/Homescreen/Homescreen.js
@@ -1,28 +1,31 @@
 import React, { useContext, useEffect } from 'react'
 import { useHistory } from "react-router-dom";
 import MainNavbar from '../Navbars/MainNavbar/MainNavbar';
-import { Flex, Container, Box, IconButton} from '@chakra-ui/react';
+import { Flex, Container, Box, IconButton } from '@chakra-ui/react';
 import { MdCreate } from 'react-icons/md';
+import { BsPencilSquare } from 'react-icons/bs'
 import HomescreenNew from './HomescreenNew';
 import HomescreenQuick from './HomescreenQuick';
 import HomescreenPopular from './HomescreenPopular';
 
 const Homescreen = (props) => {
     let history = useHistory();
-	const redirect = async(route) => {
-		history.push(route, {reload: true});
-	}
+    const redirect = async (route) => {
+        history.push(route, { reload: true });
+    }
 
     return (
         <div className='overlay'>
-            <MainNavbar redirect={redirect}/>
+            <MainNavbar redirect={redirect} />
             <Box height={'100%'} width={'100%'}>
                 <Flex gap={0} minH={'90%'} className='Homescreen-Main'>
-                    <HomescreenNew/>
-                    <HomescreenPopular/>
-                    <HomescreenQuick/>
+                    <HomescreenNew />
+                    <HomescreenPopular />
+                    <HomescreenQuick />
                 </Flex>
             </Box>
+            <IconButton id='edit-button' icon={<BsPencilSquare className='md-icon' />} bg='transparent' />
+
         </div>)
 }
 //<IconButton size='lg' bg='transparent' icon={<MdCreate className='md-icon'/>} className='create-new-button' borderRadius={30} borderColor={'black'} variant='outline'/>
