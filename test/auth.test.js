2677d93c709b9873ada7e376c77077af1f8f6e58
bladexyzz <yibo.hu@stonybrook.edu>
2022-10-30 00:03:24 -0400

    register jest

---
+++ test/auth.test.js
@@ -0,0 +1,24 @@
+import 'jest-expect-json';
+const user = require('./User.json')
+const { getLoggedIn, registerUser, loginUser, logoutUser, changePassword, updateAccount } = require('../controllers/auth-controller');
+
+
+
+
+describe("Auth controller tests", () => {
+    test('register user success', () => {
+        // arrange and act
+        var result = registerUser(user);
+
+        // assert
+        expect(result).toBe(result.jsonMatching({
+            success: true,
+            user: {
+                username,
+                email
+            }
+        }));
+    });
+
+
+})
