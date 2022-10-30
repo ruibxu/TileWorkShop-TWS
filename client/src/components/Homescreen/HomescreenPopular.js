500e4af92c79ec34223bf09ad52bcfe366bd6086
XeenProof <whosejimmy@gmail.com>
2022-10-29 20:33:19 -0400

    Homescreen mostly complete

--- client/src/components/Homescreen/HomescreenPopular.js
+++ client/src/components/Homescreen/HomescreenPopular.js
@@ -1,6 +1,4 @@
 import React, { useContext, useEffect } from 'react'
-import { useHistory } from "react-router-dom";
-import MainNavbar from '../Navbars/MainNavbar';
 import { Flex, Container, Box, Divider} from '@chakra-ui/react';
 //import { GlobalStoreContext } from '../store'
 //import ListCard from './ListCard.js'
@@ -8,10 +6,6 @@ import { Flex, Container, Box, Divider} from '@chakra-ui/react';
 //import AddIcon from '@mui/icons-material/Add';
 //import List from '@mui/material/List';
 const HomescreenPopular = (props) => {
-    let history = useHistory();
-	const redirect = async(route) => {
-		history.push(route, {reload: true});
-	}
 
     return (
         <Container minW={'46%'} className='testing'>
