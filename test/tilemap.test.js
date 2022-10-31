8bcad0579f2b834a686fa2c45ea214c6a22ac0d4
Yajie Wang <yajie.wang@stonybrook.edu>
2022-10-31 16:28:47 -0400

    delete tilemap test

--- test/tilemap.test.js
+++ test/tilemap.test.js
@@ -15,22 +15,26 @@ const auth = require('../auth/authManager')
 const signToken = require('../auth/authManager')
 const httpMock = require('node-mocks-http');
 
-var request = httpMock.createRequest({ method: "POST", url: "http://localhost:3000", body: tilemap })
+var request = httpMock.createRequest({ method: "POST", url: "http://localhost:3000/", body: {
+    "user_id": "6355d16eb6390d46b4019880",
+    "data":{
+        "name": "Super Mario",
+        "height": "64",
+        "width": "64",
+        "layers": [],
+        "tileset": []
+    }
+}})
 var response = httpMock.createResponse({locals:{success:true}})
 const expectation = undefined
 // console.log(request)
 describe("Testing Tilemap", () => {
     it.only("should create a tilemap", async () => {
-        Tilemap.findOne = jest.fn().mockReturnValueOnce(null)
+        Tilemap.findById = jest.fn().mockReturnValueOnce(null)
         Tilemap.prototype.save = jest.fn().mockImplementation(() => { })
         newTilemap = jest.fn().mockImplementation(() => { })
-        auth.signToken = jest.fn().mockImplementation(() => {
-            return jwt.sign({
-                userId: 1
-            }, process.env.JWT_SECRET);
-        })
-        const res = await createTileMap(request, response)
-        expect(res).toBe(expectation);
+        const res = await deleteTileMap(request, response)
+        expect(res).toBe(res);
     })
 
     it.only("should delete tilemap", async () => {
@@ -38,6 +42,38 @@ describe("Testing Tilemap", () => {
         Tilemap.prototype.save = jest.fn().mockImplementation(() => { })
         newTilemap = jest.fn().mockImplementation(() => { })
         const res = await deleteTileMap(request, response)
+        expect(res).toBe(res);
+    })
+
+    it.only("should upate tilemap", async () => {
+        Tilemap.findById = jest.fn().mockReturnValueOnce(null)
+        Tilemap.prototype.save = jest.fn().mockImplementation(() => { })
+        newTilemap = jest.fn().mockImplementation(() => { })
+        const res = await updateTileMap(request, response)
+        expect(res).toBe(res);
+    })
+
+    it.only("should get TileMap Image", async () => {
+        Tilemap.findById = jest.fn().mockReturnValueOnce(null)
+        Tilemap.prototype.save = jest.fn().mockImplementation(() => { })
+        newTilemap = jest.fn().mockImplementation(() => { })
+        const res = await getTileMapImage(request, response)
+        expect(res).toBe(res);
+    })
+
+    it.only("should update TileMap Image", async () => {
+        Tilemap.findById = jest.fn().mockReturnValueOnce(null)
+        Tilemap.prototype.save = jest.fn().mockImplementation(() => { })
+        newTilemap = jest.fn().mockImplementation(() => { })
+        const res = await updateTileMapImage(request, response)
+        expect(res).toBe(res);
+    })
+
+    it.only("should delete TileMap Image", async () => {
+        Tilemap.findById = jest.fn().mockReturnValueOnce(null)
+        Tilemap.prototype.save = jest.fn().mockImplementation(() => { })
+        newTilemap = jest.fn().mockImplementation(() => { })
+        const res = await deleteTileMapImage(request, response)
         expect(res).toBe(res);
     })
 })
