9ac9d7f97efd825cc62839094eac5fa3176b533f
Ruibo Xu <ruibo.xu@stonybrook.edu>
2022-10-31 11:46:14 -0400

    notification button done

--- client/src/components/Navbars/MainNavbar/LoggedIn.js
+++ client/src/components/Navbars/MainNavbar/LoggedIn.js
@@ -11,15 +11,39 @@ import {
     MenuDivider,
   } from '@chakra-ui/react';
   import { MdListAlt, MdFavoriteBorder, MdNotificationsNone, MdPersonOutline} from "react-icons/md";
+  import NewsEntry from '../../Homescreen/HomescreenNewEntry';
 
-
+  let notifications = [
+    {username: 'Yibo', action: 'created', projectName: 'Super Mario Bros 1', content: 'Super Mario Bros 1',notificationTime:'12:59 pm'},
+    {username: 'NotYibo', action: 'commented', projectName: 'Super Mario Bros 2', content: "\"nice work\"",notificationTime:'9/22/2022'},
+    {username: 'YiboLover', action: 'commented', projectName: 'Super Mario Bros 3', content: "\"nice work\"", notificationTime:'8/22/2022'},
+    {username: 'YiboHater', action: 'created', projectName: 'Super Mario Bros 4', content: 'Super Mario Bros 4',notificationTime:'8/20/2022'},
+    {username: 'WhoseYibo', action: 'created', projectName: 'Super Mario Bros 5', content: 'Super Mario Bros 5',notificationTime:'8/19/2022'},
+    {username: 'WhoseYibo', action: 'created', projectName: 'Super Mario Bros 5', content: 'Super Mario Bros 5',notificationTime:'8/19/2022'},
+    {username: 'WhoseYibo', action: 'created', projectName: 'Super Mario Bros 5', content: 'Super Mario Bros 5',notificationTime:'8/19/2022'},
+    {username: 'YiboClone', action: 'created', projectName: 'Super Mario Bros 6', content: 'Super Mario Bros 6',notificationTime:'7/1/2022'}
+]
 const LoggedIn = (props) => {
     return(
         <Flex gap={3} alignItems={'center'} width='240px' className='align-right'>
-            <IconButton icon={<MdFavoriteBorder className='md-icon'/>} bg='transparent'/>
-            <IconButton icon={<MdPersonOutline className='md-icon'/>} bg='transparent'/>
+            <IconButton icon={<MdFavoriteBorder className='md-icon'/>} onClick={()=>props.redirect('/listscreen')} bg='transparent'/>
+            <IconButton icon={<MdPersonOutline className='md-icon'/>} onClick={()=>props.redirect('/listscreen')} bg='transparent'/>
             <IconButton icon={<MdListAlt className='md-icon'/>} onClick={()=>props.redirect('/listscreen')} bg='transparent'/>
+        <Menu>
+            <MenuButton
+            as={Button}
+            variant={'link'}
+            colorScheme={'black'}>
             <IconButton icon={<MdNotificationsNone className='md-icon'/>} bg='transparent'/>
+            </MenuButton>
+
+            <MenuList overflowY={'scroll'} maxH={'300px'}>
+                {notifications.map((notification) => (
+                    <MenuItem><NewsEntry info={notification}/></MenuItem>
+                ))}
+            </MenuList>
+
+        </Menu>
         <Menu>
         <MenuButton
             as={Button}
