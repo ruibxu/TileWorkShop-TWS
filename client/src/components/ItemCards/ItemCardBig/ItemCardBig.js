6b3dc0ce4474e79520f68b042695a9ba09b4b96b
bladexyzz <yibo.hu@stonybrook.edu>
2022-10-31 15:16:37 -0400

    images

--- client/src/components/ItemCards/ItemCardBig/ItemCardBig.js
+++ client/src/components/ItemCards/ItemCardBig/ItemCardBig.js
@@ -3,7 +3,6 @@ import { useDisclosure } from "@chakra-ui/react";
 import {
     Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, Button, Textarea, Text, Icon
 } from '@chakra-ui/react'
-import image from '../../../2kfVc.png';
 import { Badge, Box, IconButton, Image, Flex, Spacer } from '@chakra-ui/react';
 import { FiThumbsUp, FiThumbsDown } from 'react-icons/fi'
 import { AiOutlineHeart } from 'react-icons/ai'
@@ -21,7 +20,7 @@ function ItemCardBig(props) {
                 <ModalContent height="800px" maxWidth="1000px">
                     <ModalHeader>
                         <Flex alignItems='center'>
-                            <Image minW={'100%'} borderRadius='lg' maxW={'100%'} height='200px' fit="none" src={image} />
+                            <Image minW={'100%'} borderRadius='lg' maxW={'100%'} height='200px' fit="none" src={data.src} />
                             <Flex gap={2} alignItems={'center'}  >
                                 <IconButton id="big-buttons" bg='transparent' icon={<AiOutlineHeart className='md-icon' />} ></IconButton>
                                 <IconButton id="big-buttons" bg='transparent' icon={<FiThumbsUp className='md-icon' />} ></IconButton>
@@ -29,9 +28,9 @@ function ItemCardBig(props) {
                             </Flex>
                             <Flex className="item-counter" gap={4} alignItems='center'>
                                 <Text fontSize={12} opacity={0.5}>{`412347`}</Text>
-                                <FiThumbsUp bg='transparent'  size={'10px'} />
+                                <FiThumbsUp bg='transparent' size={'10px'} />
                                 <Text fontSize={12} opacity={0.5}>{`14124`}</Text>
-                                <FiThumbsDown bg='transparent'  size={'10px'} />
+                                <FiThumbsDown bg='transparent' size={'10px'} />
                                 <Text fontSize={12} opacity={0.5}>{`69420`}</Text>
                                 <GoComment bg='transparent' size={'10px'} />
                             </Flex>
@@ -62,6 +61,9 @@ function ItemCardBig(props) {
                         <CommentList comments={comments} _id={data._id} />
                     </ModalBody>
                     <ModalFooter>
+                        <Button colorScheme='yellow' mr={3} onClick={() => props.onClose()}>
+                            View
+                        </Button>
                         <Button colorScheme='red' mr={3} onClick={props.openDeleteModal}>Delete</Button>
                         <Button colorScheme='blue' onClick={() => props.onClose()}>
                             Close
