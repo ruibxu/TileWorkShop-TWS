51b4265c98e7a1053dea07913cb8973a533efb45
bladexyzz <yibo.hu@stonybrook.edu>
2022-10-30 20:00:48 -0400

    itemcardbig push

--- client/src/components/ItemCards/ItemCardBig.js
+++ client/src/components/ItemCards/ItemCardBig.js
@@ -4,6 +4,8 @@ import {
 } from '@chakra-ui/react'
 import image from '../../2kfVc.png';
 import { Badge, Box, IconButton, Image, Flex, Spacer } from '@chakra-ui/react';
+import { FiThumbsUp, FiThumbsDown } from 'react-icons/fi'
+import { AiOutlineHeart } from 'react-icons/ai'
 function ItemCardBig(props) {
     const { isOpen, onOpen, onClose } = useDisclosure()
     const { data } = props
@@ -13,8 +15,13 @@ function ItemCardBig(props) {
                 <ModalOverlay />
                 <ModalContent height="500px" maxWidth="1000px">
                     <ModalHeader>
-                        <Flex alignItems='center' as="button">
-                            <Image minW={'100%'} borderRadius='lg' maxW={'100%'} height='200px' fit="none" src={image} marginRight={0} onClick={() => props.openItemCard()} />
+                        <Flex alignItems='center'>
+                            <Image minW={'100%'} borderRadius='lg' maxW={'100%'} height='200px' fit="none" src={image} marginRight={0}/>
+                        </Flex>
+                        <Flex gap={2} alignItems={'center'} width='0px' id = "big-buttons">
+                            <IconButton bg='transparent' icon={<AiOutlineHeart className='md-icon' />} ></IconButton>
+                            <IconButton bg='transparent' icon={<FiThumbsUp className='md-icon' />} ></IconButton>
+                            <IconButton bg='transparent' icon={<FiThumbsDown className='md-icon' />} ></IconButton>
                         </Flex>
                     </ModalHeader>
                     <ModalCloseButton />
