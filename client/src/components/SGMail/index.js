3a474a9d43ca859613c4ea2ee92fdb19c8528835
Yajie Wang <yajie.wang@stonybrook.edu>
2022-11-06 12:54:58 -0500

    email controller

--- client/src/components/SGMail/index.js
+++ client/src/components/SGMail/index.js
@@ -1,13 +1,14 @@
 const sgMail = require('@sendgrid/mail')
+// const SENDGRID_API_KEY = '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'
+// sgMail.setApiKey(SENDGRID_API_KEY)
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
-const sendMail = (mail) =>{
+// const sendMail = (mail) =>{
 const msg = {
-  to: mail, // Change to your recipient
+  to: 'yorkwyj@gmail.com', // Change to your recipient
   from: 'tileworkshoptws@gmail.com', // Change to your verified sender
-  subject: 'Sending with SendGrid is Fun',
-  text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  templateId: 'd-703fe27c09934dc5b217313390de64f0',
+  dynamic_template_data: {link:'www.google.com'}
 }
 sgMail
   .send(msg)
@@ -17,6 +18,6 @@ sgMail
   .catch((error) => {
     console.error(error)
   })
-}
+// }
 
-export default sendMail;
+// export default sendMail;
