c43ae2cb92d760ab0c16d2c128c6dfa856e03d7b
XeenProof <whosejimmy@gmail.com>
2022-11-13 13:38:23 -0500

    Page Numbers complete

--- client/src/components/Listscreen/ListscreenList.js
+++ client/src/components/Listscreen/ListscreenList.js
@@ -8,17 +8,17 @@ import ItemCardSmall from '../ItemCards/ItemCardSmall';
 
 const ListscreenList = (props) => {
 
-    const {data} = props
+    const {data, displayIndex} = props
 
     return (
         <Box flex='1' padding={'3%'} paddingTop={'5%'} className='identify'>
             <SimpleGrid columns={3} spacing={10} width={'100%'}className='identi'>
-                <ItemCardSmall size='100%' data={data[0]} openItemCard={props.openItemCard} redirect={props.redirect}/>
-                <ItemCardSmall size='100%' data={data[1]} openItemCard={props.openItemCard} redirect={props.redirect}/>
-                <ItemCardSmall size='100%' data={data[2]} openItemCard={props.openItemCard} redirect={props.redirect}/>
-                <ItemCardSmall size='100%' data={data[3]} openItemCard={props.openItemCard} redirect={props.redirect}/>
-                <ItemCardSmall size='100%' data={data[4]} openItemCard={props.openItemCard} redirect={props.redirect}/>
-                <ItemCardSmall size='100%' data={data[5]} openItemCard={props.openItemCard} redirect={props.redirect}/>
+                <ItemCardSmall size='100%' data={data[displayIndex-6]} openItemCard={props.openItemCard} redirect={props.redirect}/>
+                <ItemCardSmall size='100%' data={data[displayIndex-5]} openItemCard={props.openItemCard} redirect={props.redirect}/>
+                <ItemCardSmall size='100%' data={data[displayIndex-4]} openItemCard={props.openItemCard} redirect={props.redirect}/>
+                <ItemCardSmall size='100%' data={data[displayIndex-3]} openItemCard={props.openItemCard} redirect={props.redirect}/>
+                <ItemCardSmall size='100%' data={data[displayIndex-2]} openItemCard={props.openItemCard} redirect={props.redirect}/>
+                <ItemCardSmall size='100%' data={data[displayIndex-1]} openItemCard={props.openItemCard} redirect={props.redirect}/>
             </SimpleGrid>
         </Box>
     )
