ead92187c2c4db2af76f15bf9ed172affe767461
XeenProof <whosejimmy@gmail.com>
2022-11-19 16:35:55 -0500

    temp

--- models/layer-model.js
+++ models/layer-model.js
@@ -6,7 +6,7 @@ const Property = require('./property-model').schema
 const LayerSchema = new Schema(
     {
         name: { type: String, required: true },
-        data: { type: [Number], required: true },
+        data: { type: [Object], required: true },
         properties: { type:[Property], required: true },
         locked: {type: Boolean, required: true},
         hidden: {type: Boolean, required: true}
