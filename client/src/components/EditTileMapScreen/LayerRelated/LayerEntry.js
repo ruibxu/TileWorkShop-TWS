2d49c212e82a9ca484ef52598cb652848662139d
XeenProof <whosejimmy@gmail.com>
2022-11-20 12:32:22 -0500

    Layer changes completed

--- client/src/components/EditTileMapScreen/LayerRelated/LayerEntry.js
+++ client/src/components/EditTileMapScreen/LayerRelated/LayerEntry.js
@@ -13,34 +13,52 @@ const LayerEntry = (props) => {
     const currentLayer = props.currentLayer
     const [edit, toggleEdit] = useState(false)
     const [rename, setRename] = useState(name)
+    const [hide, toggleHide] = useState(hidden)
+    const [lock, toggleLock] = useState(locked)
+
+    const handleMakeTransaction = (name, value) => {
+        const layersClone = JSON.parse(JSON.stringify(editStore.layers))
+        layersClone.map(x => x[name]=(x.id == id)?value:x[name])
+        editStore.addLayerStateTransaction(layersClone)
+    }
+
     const handleSelect = () => {
         props.setCurrentLayer(id)
     }
 
-    const handleRename = () => {
+    const handleToggleRename = () => {
         toggleEdit(true)
     }
 
     const handleFinishRename = (e) => {
+        toggleEdit(false)
         if(e.target.value == rename){return}//only renames when the name actually changes
         const newName = e.target.value
         setRename(newName)
-        const layersClone = JSON.parse(JSON.stringify(editStore.layers))
-        layersClone.map(x => x.name=(x.id == id)?newName:x.name)
-        editStore.addLayerStateTransaction(layersClone)
-        toggleEdit(false)
+        handleMakeTransaction('name', newName)
     }
 
-    useEffect(()=>{
+    const handleToggleHide = () => {
+        handleMakeTransaction('hidden', !hide)
+    }
+
+    const handleToggleLock = () => {
+        handleMakeTransaction('locked', !lock)
+    }
+
+    useEffect(()=>{//for undo redo purposes
         const reset = editStore.layers.find(x => x.id == id)
         if(!reset){return}
         if(reset.name != rename){
             setRename(reset.name)
+            toggleEdit(false)
         }
+        if(reset.hidden != hide){toggleHide(reset.hidden)}
+        if(reset.locked != lock){toggleLock(reset.locked)}
     },[editStore.layers])
 
     return (<Box height='35px' width={'100%'} className={(currentLayer == id)?'layer-entry-selected':'layer-entry'} id={id}>
-        <Flex height='100%' width={'100%'} alignItems='center' onClick={handleSelect} onDoubleClick={handleRename}>
+        <Flex height='100%' width={'100%'} alignItems='center' onClick={handleSelect} onDoubleClick={handleToggleRename}>
             {(edit)?
             <Box onBlur={handleFinishRename}>
                     <Input defaultValue={rename} autoFocus={true}/>
@@ -49,8 +67,10 @@ const LayerEntry = (props) => {
                 {rename?rename:'unnamed Layer'}
             </Box>}
             <Spacer/>
-            <IconButton bg='transparent' title="Hide" icon={(hidden)?<HiEyeOff/>:<HiEye />} maxH='30px'/>
-            <IconButton bg='transparent' title="Lock" icon={(locked)?<AiFillLock/>:<AiFillUnlock/>} maxH='30px'/>
+            <IconButton bg='transparent' title="Hide" icon={(hide)?<HiEyeOff/>:<HiEye />} maxH='30px'
+                onClick={handleToggleHide}/>
+            <IconButton bg='transparent' title="Lock" icon={(lock)?<AiFillLock/>:<AiFillUnlock/>} maxH='30px'
+                onClick={handleToggleLock}/>
         </Flex>
     </Box>)
 }
