16ef57ac38bba250ef9e91ac76b611cf29cd7501
bladexyzz <yibo.hu@stonybrook.edu>
2022-11-21 16:36:44 -0500

    open file

--- client/src/components/Navbars/EditNavbar.js
+++ client/src/components/Navbars/EditNavbar.js
@@ -1,21 +1,21 @@
 import React, { useState, useContext, useEffect } from 'react'
 import logo from '../../logo_cropped.png';
 import {
-  Box,
-  Flex,
-  HStack,
-  Image,
-  IconButton,
-  Input,
-  Select,
-  Menu,
-  MenuButton,
-  Button,
-  Avatar,
-  MenuList,
-  MenuItem,
-  MenuDivider,
-  Switch,
+    Box,
+    Flex,
+    HStack,
+    Image,
+    IconButton,
+    Input,
+    Select,
+    Menu,
+    MenuButton,
+    Button,
+    Avatar,
+    MenuList,
+    MenuItem,
+    MenuDivider,
+    Switch,
 } from '@chakra-ui/react';
 import { BiSave } from "react-icons/bi";
 import { MdFolderOpen, MdOutlineFileDownload } from 'react-icons/md'
@@ -32,7 +32,7 @@ const EditNavbar = (props) => {
         props.setPublic(v)
     }
 
-    const handleLogout = () =>{
+    const handleLogout = () => {
         props.redirect('/homescreen')
     }
 
@@ -43,52 +43,61 @@ const EditNavbar = (props) => {
 
     return (
         <Box px={4} className="navbar" left={0}>
-        <HStack h={16} justifyContent={'space-between'}>
-            <Flex alignItems={'center'} gap={5}>
-                <Box as= "button"><Image src={logo} maxH='50px' objectFit='fill' onClick={()=>props.redirect('/homescreen')}/></Box>
-                <IconButton bg='transparent' icon={<MdFolderOpen className='md-icon'/>}/>
-                <IconButton bg='transparent' icon={<BiSave className='md-icon'/>}/>
-                <IconButton bg='transparent' icon={<MdOutlineFileDownload className='md-icon'/>}/>
-            </Flex>
-            <Flex>
-                {(nameEdit)?
-                <Box alignItems={'center'} className='name-font' onBlur={handleNameEdit}>
-                    <Input defaultValue={name} autoFocus={true}/>
-                </Box>
-                :<Box alignItems={'center'} className='name-font' onClick={()=>toggleNameEdit(!nameEdit)}>
-                    {name}
-                </Box>}
-            </Flex>
-            <Flex gap={6} alignItems={'center'} width="width='240px">
-                <Flex gap={4} alignItems={'center'}>
-                    <Button variant={'solid'} colorScheme={(props.isPublic)?"green":"red"} onClick={()=>handleSetPublic(!props.isPublic)} width='75px'>
-                                {(props.isPublic)?"Public":"Private"}
-                    </Button>
-                    <Button variant={'solid'} colorScheme={'blue'}>Edit</Button>
-                    <Button variant={'solid'} colorScheme={'blue'} onClick={props.openShareModal}>Share</Button>
+            <HStack h={16} justifyContent={'space-between'}>
+                <Flex alignItems={'center'} gap={5}>
+                    <Box as="button"><Image src={logo} maxH='50px' objectFit='fill' onClick={() => props.redirect('/homescreen')} /></Box>
+                    <Box>
+                        <IconButton bg='transparent' icon={<MdFolderOpen className='md-icon' />} onClick={() => document.querySelector('input').click()} />
+                        <Input
+                            type="file"
+                            style = {{display:"none"}}
+                            accept="image/*"
+                            id='input'
+                        />
+
+                    </Box>
+                    <IconButton bg='transparent' icon={<BiSave className='md-icon' />} />
+                    <IconButton bg='transparent' icon={<MdOutlineFileDownload className='md-icon' />} />
                 </Flex>
-                <Menu>
-                <MenuButton
-                    as={Button}
-                    rounded={'full'}
-                    variant={'link'}
-                    cursor={'pointer'}
-                    minW={0}>
-                    <Avatar
-                    size={'sm'}
-                    name={(auth.loggedIn)?auth.user.username:'not logged in'}
-                    />
-                </MenuButton>
-                <MenuList>
-                    <MenuItem>{(auth.loggedIn)?auth.user.username:''}</MenuItem>
-                    <MenuDivider />
-                    <MenuItem>Update</MenuItem>
-                    <MenuItem onClick={handleLogout}>Logout</MenuItem>
-                </MenuList>
-                </Menu>
-            </Flex>
-        </HStack>
-      </Box>)
+                <Flex>
+                    {(nameEdit) ?
+                        <Box alignItems={'center'} className='name-font' onBlur={handleNameEdit}>
+                            <Input defaultValue={name} autoFocus={true} />
+                        </Box>
+                        : <Box alignItems={'center'} className='name-font' onClick={() => toggleNameEdit(!nameEdit)}>
+                            {name}
+                        </Box>}
+                </Flex>
+                <Flex gap={6} alignItems={'center'} width="width='240px">
+                    <Flex gap={4} alignItems={'center'}>
+                        <Button variant={'solid'} colorScheme={(props.isPublic) ? "green" : "red"} onClick={() => handleSetPublic(!props.isPublic)} width='75px'>
+                            {(props.isPublic) ? "Public" : "Private"}
+                        </Button>
+                        <Button variant={'solid'} colorScheme={'blue'}>Edit</Button>
+                        <Button variant={'solid'} colorScheme={'blue'} onClick={props.openShareModal}>Share</Button>
+                    </Flex>
+                    <Menu>
+                        <MenuButton
+                            as={Button}
+                            rounded={'full'}
+                            variant={'link'}
+                            cursor={'pointer'}
+                            minW={0}>
+                            <Avatar
+                                size={'sm'}
+                                name={(auth.loggedIn) ? auth.user.username : 'not logged in'}
+                            />
+                        </MenuButton>
+                        <MenuList>
+                            <MenuItem>{(auth.loggedIn) ? auth.user.username : ''}</MenuItem>
+                            <MenuDivider />
+                            <MenuItem>Update</MenuItem>
+                            <MenuItem onClick={handleLogout}>Logout</MenuItem>
+                        </MenuList>
+                    </Menu>
+                </Flex>
+            </HStack>
+        </Box>)
 }
 
 export default EditNavbar;
