276265386410414a2ac95db2943bd48e9f0bf6af
Ruibo Xu <ruibo.xu@stonybrook.edu>
2022-12-01 23:49:16 -0500

    export

--- client/src/import-export/exportTM.js
+++ client/src/import-export/exportTM.js
@@ -6,46 +6,51 @@ const exportTM = (store) => {
     //const  jsonClone= JSON.parse(JSON.stringify(store))
 
     const exportingJson={
-        "compressionlevel":-1,
-        "height":store.height,
-        "infinite":false,
-        // need to change
-        "layers":[
-            { 
-                "data":[],
-                "height":store.height,
-                "id":1,
-                "name":"background",
-                "opacity":1,
-                "type":"tilelayer",
-                "visible":true,
-                "width":store.width,
-                "x":0,
-                "y":0
-            },
-        ],
-        "orientation":"orthogonal",
-        "renderorder":"right-down",
-        "tileheight":store.scale,// question
-        // need to change
-        "tilesets":[
-                {
-                "columns":10,
-                "firstgid":1,
-                "image":"Hello.png",
-                "imageheight":320,
-                "imagewidth":320,
-                "margin":0,
-                "name":"tileset",
-                "spacing":0,
-                "tilecount":100,
-                "tileheight":32,
-                "tilewidth":32
-                }],
-        "tilewidth":store.scale,
-        "type":"map",
-        "width":store.width
+        compressionlevel:-1,
+        height:store.height,
+        infinite:false,
+        layers:null,
+        orientation:"orthogonal",
+        renderorder:"right-down",
+        tileheight:null,
+        tilesets:null,
+        tilewidth:null,
+        type:"map",
+        width:store.width
     }
+    if(store.tilesets[0]){
+        exportingJson.tilewidth=store.tilesets[0].pixel;
+        exportingJson.tileheight=store.tilesets[0].pixel;
+    }
+
+
+    //handle tileset
+    let tmp_arr2=[];
+    let index=1;
+    let counting_array=[]
+    store.tilesets.forEach((tileset) => {
+        const temp = translateTileset(tileset,store,index)
+        tmp_arr2.push(temp)
+        index+=(tileset.height*tileset.width);
+        counting_array.push(index);
+    })
+
+    exportingJson.tilesets=tmp_arr2
+
+
+
+    //handle layer
+    let tmp_arr=[];
+    store.layers.forEach((layer) => {
+        
+        const temp = translateLayer(layer,store,counting_array)
+        tmp_arr.push(temp)
+        //translatedlayerarray.push(temp)
+    })
+    exportingJson.layers=tmp_arr
+
+
+
     
     const json = JSON.stringify(exportingJson, null, 2);
     const blob = new Blob([json], { type: "application/json" });
@@ -68,14 +73,86 @@ export default exportTM;
 //     translatedlayerarray.push(temp)
 // })
 
-// translateLayer = (x) => {
-//     for(let x = 0, x < tilemapwidth; x++){
-//         for(let y = 0, y < tilemapheight; y++){
-//             key = `${x}-${y}`
-//         }
-//     }
-//     return translatedX
-// }
+const translateLayer = (layer,store,counting_array) => {
+
+    console.log(layer.data)
+    //
+    let current_layer={
+        data:null,
+        height:store.height,
+        id:layer.id,
+        name:layer.name,
+        opacity:1,
+        properties:null,
+        type:"tilelayer",
+        visible:!layer.hidden,
+        width:store.width,
+        x:0,
+        y:0
+    };
+    //layer
+    let temp_prop=[];
+    layer.properties.forEach((property) => {
+        const temp = translateProperty(property)
+        temp_prop.push(temp)
+    })
+    current_layer.properties=temp_prop
+
+    //data
+    let tileArray=[];
+    for(let y = 0; y < store.height; y++){
+        for(let x = 0; x < store.width; x++){
+            let key = `${x}-${y}`;
+            if(layer.data[key]==null){
+                tileArray.push(0);
+            }
+            else{
+                let index=0;
+                for(let i = 0; i < counting_array.length; i++){
+                    let sum=0;
+                    sum=layer.data[key][0]+store.tilesets[i].width*layer.data[key][1]+1+sum;
+                    if(sum<counting_array[i]){
+                        index=i;
+                        break;
+                    }
+                }
+                tileArray.push(layer.data[key][0]+store.tilesets[index].width*layer.data[key][1]+1)
+            }
+        }
+    }
+    current_layer.data=tileArray;
+
+
+    return current_layer
+}
+
+const translateProperty = (property) => {
+    let current_property={
+        name:property.name,
+        type:property.type,
+        value:property.value
+    };
+    return current_property;
+}
+
+
+const translateTileset = (tileset,store,index) => {
+    let current_tilset={
+        columns:tileset.width,
+        firstgid:index,
+        image:`${tileset.name}.png`,
+        imageheight:tileset.height*tileset.pixel,
+        imagewidth:tileset.width*tileset.pixel,
+        margin:0,
+        name: tileset.name,
+        spacing:0,
+        tilecount:tileset.height*tileset.width,
+        tileheight:tileset.pixel,
+        tilewidth:tileset.pixel
+    };
+    return current_tilset;
+}
+
 
 // valueformat = [tilesetX, tilesetY, TilesetID]
 // (tilesetX * tilesetWidth)+tilesetY + startingValue
