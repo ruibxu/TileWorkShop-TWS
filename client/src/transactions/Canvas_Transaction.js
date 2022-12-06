8979c8fe53ca577392a0f4775cb4a23bebe45389
XeenProof <whosejimmy@gmail.com>
2022-12-06 11:20:52 -0500

    removed some scrap

--- client/src/transactions/Canvas_Transaction.js
+++ client/src/transactions/Canvas_Transaction.js
@@ -5,21 +5,16 @@ export default class Canvas_Transcation extends jsTPS_Transaction {
         super();
         this.undoCallback = undoCallback
         this.redoCallback = redoCallback
-        // this.oldState = initOldState;
-        // this.newState = initNewState;
-        // this.setFunction = setFunction
         console.log('transaction created')
     }
 
     doTransaction() {
         console.log('Reached Do Transaction')
         this.redoCallback()
-        // this.setFunction(this.newState)
     }
 
     undoTransaction() {
         console.log('Reached undo Transaction')
         this.undoCallback()
-        // this.setFunction(this.oldState)
     }
 }
