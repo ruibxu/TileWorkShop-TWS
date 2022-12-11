9736fcc616a306c4d542dcb465533845b1e571a7
bladexyzz <yibo.hu@stonybrook.edu>
2022-12-11 13:20:38 -0500

    cloudinary bgfx

--- controllers/cloudinary-controller.js
+++ controllers/cloudinary-controller.js
@@ -19,8 +19,8 @@ const getTileMapImage = async (req, res) => {
 const getRelatedTileSets = async (req, res) => {
     const tilemap_id = req.params.id;
     const search = `folder:TileMap_Uses AND tags=${tilemap_id} OR folder:TileMap_Uses/${tilemap_id}`
-    const { resources }= await cloudinary.v2.search.expression(search).execute();
-    if(!resources){
+    const { resources } = await cloudinary.v2.search.expression(search).execute();
+    if (!resources) {
         return res.status(201).json({
             errorMessage: 'images not found!',
         });
@@ -36,21 +36,13 @@ const deleteTileMap = async (req, res) => {
     const search = `folder:TileMap_Uses AND tags=${tilemap_id} OR folder:TileMap_Uses/${tilemap_id}`
     const searchFolder = `TileMap_Uses/${tilemap_id}`
     const deleteTileMap = `TileMap_Thumbnail/${tilemap_id}`
-    const response2 = await cloudinary.v2.uploader.destroy(deleteTileMap).catch(error => {console.log(error)})
-    const response3 = await cloudinary.v2.api.delete_resources_by_tag(tilemap_id).catch(error => {console.log(error)});
-    // return res.status(200).json({message: "worked", id: tilemap_id})
-    const response = await cloudinary.v2.api.delete_folder(searchFolder).catch(error => {console.log(error)})
-    .catch(error => 
-        {return res.status(200).json({
-            message:"No folder to delete",
-            error
-        })}
-    )
+    const response2 = await cloudinary.v2.uploader.destroy(deleteTileMap).catch(error => { console.log("Tilemap not found") })
+    const response3 = await cloudinary.v2.api.delete_resources_by_tag(tilemap_id).catch(error => { console.log("Tilemap id not found") });
+    const response = await cloudinary.v2.api.delete_folder(searchFolder).catch(error => {console.log("Folder not found")})
     return res.status(200).json({
-        _id: tilemap_id,
-        resources: response,
-        //resources2: response2.resources
+        _id: tilemap_id
     })
+
 }
 
 const updateTileMapImage = async (req, res) => {
@@ -84,7 +76,7 @@ const deleteTileMapImage = async (req, res) => {
     if (resources.result === "not found") {
         const search2 = `TileMap_Uses/${public_id}`
         const resources2 = await cloudinary.v2.uploader.destroy(search2)
-        if(resources2.result === "not found"){
+        if (resources2.result === "not found") {
             return res.status(201).json({
                 errorMessage: 'image not found!',
             });
@@ -185,7 +177,7 @@ const deleteTileSetImage = async (req, res) => {
     const public_id = req.params.id;
     const search = `TileSet_Editor/${public_id}`;
     // console.log(search)
-    const resources  = await cloudinary.v2.uploader.destroy(search);
+    const resources = await cloudinary.v2.uploader.destroy(search);
     if (resources.result === "not found") {
         return res.status(201).json({
             errorMessage: 'image not found!',
